326602a742ba49088dd9441fe5940bed86ebd656
Siitoo <alfonsosanchez1896@gmail.com>
2019-11-11 19:27:40 +0100

    First iteration of movement

---
+++ FunRace3D/Assets/Scenes/Scripts/ObstaclePath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePath : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector3 direction = Vector3.zero;
+
+    private void Start()
+    {
+        switch(tag)
+        {
+            case "XPath":
+                direction = Vector3.right;
+                break;
+            case "YPath":
+                direction = Vector3.up;
+                break;
+            case "ZPath":
+                direction = Vector3.forward;
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().direction = direction;
+        }
+    }
+
+}
+
+
